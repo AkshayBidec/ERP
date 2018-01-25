203ffaf0283fda36d8457e7f4e96da9db6c754b3
Nitish <mr.nitishkumar@outlook.com>
2018-01-25 18:46:51 +0530

    controller> DashBoard, LoginPage

--- controllers/DashBoard.py
+++ controllers/DashBoard.py
@@ -1,3 +1,13 @@
-# -*- coding: utf-8 -*-
-# try something like
-def index(): return dict(message="hello from DashBoard.py")
+# this contr. contain all the dashbords, SA dashboard and general 
+
+#==============================================================================
+#function: dashboard_SA
+#dis: it is the special dashboard for the SA and will include the special setting 
+def dashboard_SA():
+	return locals()
+
+#==============================================================================
+#function: dashboard
+#dis: this is the general dashboard and will have the limited functionalities
+def dashboard():
+	return locals()
--- controllers/LoginPage.py
+++ controllers/LoginPage.py
@@ -1,3 +1,31 @@
-# -*- coding: utf-8 -*-
-# try something like
-def index(): return dict(message="hello from LoginPage.py")
+# this page include all the login pages 
+
+#==============================================================================
+#function: first_time_login_SA
+#dis: for the first time login of the super admin, should run only once 
+#	  and contain basic tutorial for the app setup
+# will be sent to the dashboard from this
+def first_time_login_SA():
+	return locals()
+
+#==============================================================================
+#function: login_SA
+#dis:for the general login of the super ardmin
+# will be sent to the dashboard from this
+def login_SA():
+	return locals()
+
+#==============================================================================
+#function: first_time_login
+#dis: for the first time login of the general user, should run only once 
+#	  and contain basic tutorial for the app and will only appear after the SA validation
+# will be sent to the dashboard from this
+def first_time_login():
+	return locals()
+
+#==============================================================================
+#function: login
+#dis:for the general user login
+# will be sent to the dashboard from this
+def login():
+	return locals()
