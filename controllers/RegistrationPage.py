2d69880c38b594511c4045ae8b76c9a0890f0d7e
Nitish <mr.nitishkumar@outlook.com>
2018-01-25 18:19:49 +0530

    controller>RegistrationPage

---
+++ controllers/RegistrationPage.py
@@ -0,0 +1,16 @@
+# this page is the company and superuser registration page for the 1st time app run
+# this also include the custon registration page of normal user
+
+#this function should include a mothord to check that it runs only for 1 time
+#it can contain multiple view pages according to the need
+def company_reg_page():
+    return locals()
+
+# this function is only for the superuser registration 
+# it gets redirected to the 1st time login and tutorial page
+def superuser_reg_page():
+    return locals()
+
+# this page can be motified from the superuser
+def general_reg_page():
+    return locals()
